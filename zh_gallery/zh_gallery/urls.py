8ac97584ab89e7004d47b348d06b188aceecbc53
voltov-tom <voltov-tom@mail.ru>
2021-12-26 20:26:17 +0400

    project's template

---
+++ zh_gallery/zh_gallery/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.urls import path
+from django.conf import settings
+
+from core.views import base_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', base_view, name='base_view'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
