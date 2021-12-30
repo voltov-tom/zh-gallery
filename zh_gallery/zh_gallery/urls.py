a72bc01dd3ded1cb3d6825875c95938f61d7d27f
voltov-tom <voltov-tom@mail.ru>
2021-12-30 11:07:52 +0400

    some changes

--- zh_gallery/zh_gallery/urls.py
+++ zh_gallery/zh_gallery/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('<slug:slug>/', category_view, name='category_view'),
 
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
