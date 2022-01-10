32e947179d5ea1f44f06b60d783865d71ba4d893
voltov-tom <voltov-tom@mail.ru>
2022-01-10 21:54:29 +0400

    tested google auth, add template, edit css

--- zh_gallery/core/context_processors.py
+++ zh_gallery/core/context_processors.py
@@ -1,12 +1,9 @@
 from .models import MainCategory, SubCategory
-from zh_gallery import settings
 
 
 def categories(request):
     categories = MainCategory.objects.all()
-    client_id = settings.OAuth_google
     context = {
-        'client_id': client_id,
         'categories': categories
     }
     return context
