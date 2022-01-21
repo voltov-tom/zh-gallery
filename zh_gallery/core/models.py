05d1b25f7b9f0a31539f629c1d39898639fb5712
root <root@e7cfa4b601be>
2022-01-21 10:53:41 +0000

    null=True for likes

--- zh_gallery/core/models.py
+++ zh_gallery/core/models.py
@@ -66,7 +66,7 @@ class MediaItem(models.Model):
     image = models.ImageField()
 
     views = models.IntegerField(default=0)
-    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    likes = models.ManyToManyField(User, related_name='likes', null=True, blank=True)
     like_time = models.DateTimeField(default=timezone.now())
 
     def save(self, *args, **kwargs):
