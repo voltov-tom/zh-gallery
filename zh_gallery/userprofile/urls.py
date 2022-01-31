e3242e65baf6045a4303bb8c1b879d4f049ea887
voltov-tom <voltov-tom@mail.ru>
2022-01-31 17:03:51 +0400

    working with forms, editing templates

--- zh_gallery/userprofile/urls.py
+++ zh_gallery/userprofile/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import my_account
 
 urlpatterns = [
-    path('accounts/my-account/<str:username>', my_account, name='my_account_view'),
+    path('my-account/', my_account, name='my_account_view'),
 ]
