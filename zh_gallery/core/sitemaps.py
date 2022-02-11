07fb3aeb8d1eb195dd16af6b99265a6500163589
voltov-tom <voltov-tom@mail.ru>
2022-02-12 01:15:07 +0400

    add sitemaps.py, edit urls.py

---
+++ zh_gallery/core/sitemaps.py
@@ -0,0 +1,48 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+from .models import MainCategory, SubCategory, MediaItem
+
+
+def lastmod(obj):
+    return obj.date_added
+
+
+class StaticViewSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.5
+
+    def items(self):
+        return [
+            'frontpage_view',
+            'about_view',
+            'terms_and_conditions_view',
+            'privacy_policy_view'
+        ]
+
+    def location(self, item):
+        return reverse(item)
+
+
+class MainCategorySitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.5
+
+    def items(self):
+        return MainCategory.objects.all()
+
+
+class SubCategorySitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.5
+
+    def items(self):
+        return SubCategory.objects.all()
+
+
+class MediaItemSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.5
+
+    def items(self):
+        return MediaItem.objects.all()
--- zh_gallery/core/urls.py
+++ zh_gallery/core/urls.py
@@ -1,14 +1,25 @@
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path
 
+from .sitemaps import StaticViewSitemap, SubCategorySitemap, MediaItemSitemap, MainCategorySitemap
 from .views import frontpage_view, about_view, terms_and_conditions_view, privacy_policy_view, category_view, \
     subcategory_view, media_item_view
 from .api import like_button, delete_review, edit_review
+
+sitemaps = {
+    'static': StaticViewSitemap,
+    'category': MainCategorySitemap,
+    'subcategory': SubCategorySitemap,
+    'media_item': MediaItemSitemap,
+}
 
 urlpatterns = [
     path('', frontpage_view, name='frontpage_view'),
     path('about/', about_view, name='about_view'),
     path('terms-and-conditions/', terms_and_conditions_view, name='terms_and_conditions_view'),
     path('privacy-policy/', privacy_policy_view, name='privacy_policy_view'),
+
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 
     path('api/like/', like_button, name='like_button'),
     path('api/delete-review/', delete_review, name='delete_review'),
